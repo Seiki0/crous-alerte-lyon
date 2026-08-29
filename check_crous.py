21fa2cb9ca152feaba95f35ee7a81464661a9906
Seiki0 <157994204+Seiki0@users.noreply.github.com>
2026-08-30 01:41:00 +0200

    Update check_crous.py

--- check_crous.py
+++ check_crous.py
@@ -5,6 +5,8 @@ import re
 URL = "https://trouverunlogement.lescrous.fr/tools/47/search?occupationModes=alone&bounds=4.782253280282021_45.805392737249164_4.9058494716882715_45.695192390203836"
 
 r = requests.get(URL, timeout=30, headers={"User-Agent": "Mozilla/5.0"})
+r.encoding = r.apparent_encoding
+
 soup = BeautifulSoup(r.text, "html.parser")
 text = soup.get_text(" ", strip=True)
 text = re.sub(r"s+", " ", text).strip()
